4de25c01f4d15fc0b5f2919323dc0adc822594cf
Kemjiga <kemerj@uninorte.edu.co>
2023-10-04 23:02:15 -0500

    fixed structure
    cleaning project structure

--- ejec.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-lex LAB01.l
-gcc lex.yy.c -o LAB01
-./LAB01 entrada1.txt>salida1.txt
-./LAB01 entrada2.txt>salida2.txt
-./LAB01 entrada3.txt>salida3.txt
-./LAB01 entrada4.txt>salida4.txt
-echo "Ejecucion terminada"
